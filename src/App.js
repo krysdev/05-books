06f38763d3799d155d8374b1f8f31a7c3958060e
KrysDev <webdevkrys@gmail.com>
2023-01-31 21:33:12 +0000

    x

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 // import 'bulma/css/bulma.css';
+import { useState } from 'react';
+import BookCreate from './components/BookCreate';
 
 function App() {
+  const [books, setBooks] = useState([]);
+
+  const createBook = (title) => {  // receives title from BookCreate.js
+    console.log('Title from CREATE:', title)
+
+  };
+
   return (
-    <div className="footer">
-      -x-x-x-
+    <div>
+      <BookCreate onCreate={createBook}/>
     </div>
   );
 }
