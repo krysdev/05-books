a3c3b8bb28ec633218b6302c6b2762051b13f358
KrysDev <webdevkrys@gmail.com>
2023-02-04 22:47:02 +0000

    x

--- src/App.js
+++ src/App.js
@@ -4,14 +4,23 @@ import BookCreate from './components/BookCreate';
 function App() {
   const [books, setBooks] = useState([]);
 
-  const createBook = (title) => {  // receives title from BookCreate.js
-    console.log('Title from CREATE:', title)
+  const random = Math.round(Math.random() * 99999)
 
+  const createBook = (title) => {    // receives title from BookCreate.js (it is called 'word' there)
+    // console.log('Title/word from CREATE:', title)
+    const updatedBooksArrayState = [
+      ...books,
+      { id: random, title: title },
+    ];
+    setBooks(updatedBooksArrayState);
   };
 
+  console.log(books);
+  
   return (
-    <div>xxx
-      <BookCreate onCreate={createBook}/>
+    <div>
+      books.length: {books.length}
+      <BookCreate onCreate={createBook} />
     </div>
   );
 }
